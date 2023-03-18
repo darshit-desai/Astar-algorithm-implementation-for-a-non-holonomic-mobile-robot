f252dba9454b481b4b4014eec00fc506d160ca7b
DARSHIT DESAI <desai0681@gmail.com>
2023-03-17 23:43:41 -0400

    Update robot_move.py

--- robot_move.py
+++ robot_move.py
@@ -1,7 +1,23 @@
 import numpy as np
+import math
 # Move the robot up
-def robot_move_theta(robot,l,theta):
-    x,y=robot
-    robot=(x+l*np.cos(theta),y+l*np.sin(theta))
-    cost=1
-    return cost,robot,theta
+stepsize = 10
+xg = 90
+yg = 90
+def move_robot(x,y,curr_theta, totalcst):
+    new_nodes = []
+    for t in range(-60,61,30):
+        x_t,y_t, t_t, c2g, c2c=  actions(x,y,t,curr_theta, totalcst)
+        new_nodes.append([c2g+c2c,c2c,c2g,(x_t,y_t,t_t)])
+    return new_nodes
+
+def euclidean(x,y,xg,yg):
+    return math.dist((x,y),(xg,yg))
+
+def actions(x,y,t,ct, tc):
+    xr,yr=(x+stepsize*np.cos(t+ct),y+stepsize*np.sin(t+ct))
+    c2g = euclidean(xr,yr,xg,yg)
+    c2c = tc - c2g
+    return xr,yr,t+ct,c2g,c2c
+
+
